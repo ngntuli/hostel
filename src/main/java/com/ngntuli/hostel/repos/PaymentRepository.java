43b8ea23687945801120e6928b98aedcecb358e9
ngntuli <mr.ng.ntuli@gmail.com>
2022-12-28 23:56:55 +0200

    change jpa to crud

--- src/main/java/com/ngntuli/hostel/repos/PaymentRepository.java
+++ src/main/java/com/ngntuli/hostel/repos/PaymentRepository.java
@@ -1,7 +1,7 @@
 package com.ngntuli.hostel.repos;
 
 import com.ngntuli.hostel.models.Payment;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface PaymentRepository extends JpaRepository<Payment, Long> {
+public interface PaymentRepository extends CrudRepository<Payment, Long> {
 }
