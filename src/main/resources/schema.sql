1dcad1ff8359195b3900e68a430d61e165ab7c2a
ngntuli <mr.ng.ntuli@gmail.com>
2022-12-29 23:55:50 +0200

    update schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,11 @@
 create table if not exists payment (
     id      serial primary key ,
-    month   varchar(50),
+    paydate  timestamp,
     amount  bigint
+);
+
+create table if not exists tenant (
+    id      serial primary key ,
+    tname varchar(255),
+    room int4
 );
