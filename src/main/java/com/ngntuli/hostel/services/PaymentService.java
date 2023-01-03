bcd8040e177d9b3864df38d5b7170c3d806b2ae0
ngntuli <mr.ng.ntuli@gmail.com>
2023-01-03 22:06:59 +0200

    create function

--- src/main/java/com/ngntuli/hostel/services/PaymentService.java
+++ src/main/java/com/ngntuli/hostel/services/PaymentService.java
@@ -3,4 +3,7 @@ package com.ngntuli.hostel.services;
 import com.ngntuli.hostel.models.Payment;
 
 public interface PaymentService {
+    void create(Long tenantId, Payment payment);
+
+    Iterable<Payment> list(Long tenantId);
 }
